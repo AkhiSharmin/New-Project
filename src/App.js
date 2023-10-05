51276186155e0c0cbb598da618d4e35dd4c64597
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-05 19:52:01 +0600

    some of update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import { useState } from "react";
+
+const message = [
+  'Learn React',
+  'Apply for jobs',
+  'Invest your new income'
+];
+
 export default function App() {
-  return <div className="steps">
-    <div className="numbers">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-    </div>
-  </div>
+
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handelPrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handelNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
+
+
+
+  return <>
+
+    <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
+
+    {isOpen &&
+      <div className="steps">
+        <div className="numbers">
+          <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+          <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+          <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        </div>
+
+        <p className="message">Step {step}: {message[step - 1]}</p>
+
+        <div className="buttons">
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            onClick={handelPrevious}
+          // onMouseEnter={() => alert("TEST")}
+          >Previous</button>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            onClick={handelNext}
+          >Next</button>
+        </div>
+      </div>
+    }
+  </>
 }
