aff0f89bf84ed87e1b41eb08820bc3dac429f0e8
AkhiSharmin <akhisharmin100@gmail.com>
2023-10-10 20:41:31 +0600

    some updated

--- src/App.js
+++ src/App.js
@@ -40,15 +40,25 @@ export default function App() {
         <p className="message">Step {step}: {message[step - 1]}</p>
 
         <div className="buttons">
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
-            onClick={handelPrevious}
-          // onMouseEnter={() => alert("TEST")}
-          >Previous</button>
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}
-            onClick={handelNext}
-          >Next</button>
+
+
+          <Button bgColor="#7950f2" textColor="#fff" text="previous" onClick={handelPrevious} />
+
+          <Button bgColor="#7950f2" textColor="#fff" text="Next" onClick={handelNext} />
+
+
         </div>
       </div>
     }
   </>
+}
+
+
+function Button({ textColor, bgColor, onClick, text }) {
+  return (
+    <button style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}>
+      {text}
+    </button>
+  )
 }
